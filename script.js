724da36976186456a5f8ae5d8df89f2b41f08375
petrsonTD <petsontd@gmail.com>
2022-07-05 21:53:11 +0200

    js + css animations

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
-const home = document.querySelector("#buttonHome")
-const myDashboard = document.querySelector("#buttonMyDashboard")
-const people = document.querySelector("#buttonPeople")
-const performance = document.querySelector("#buttonPerformance")
+// Buttons
 
-home.addEventListener("click", e => {
-    home.classList.replace("inactive", "active");
-    myDashboard.classList.replace("active", "inactive");
-    people.classList.replace("active", "inactive");
-    performance.classList.replace("active", "inactive");
+const buttonHome = document.querySelector("#buttonHome")
+const buttonMyDashboard = document.querySelector("#buttonMyDashboard")
+const buttonPeople = document.querySelector("#buttonPeople")
+const buttonPerformance = document.querySelector("#buttonPerformance")
+
+buttonHome.addEventListener("click", e => {
+    buttonHome.classList.replace("inactive", "active");
+    buttonMyDashboard.classList.replace("active", "inactive");
+    buttonPeople.classList.replace("active", "inactive");
+    buttonPerformance.classList.replace("active", "inactive");
 })
 
-myDashboard.addEventListener("click", e => {
-    home.classList.replace("active", "inactive");
-    myDashboard.classList.replace("inactive", "active");
-    people.classList.replace("active", "inactive");
-    performance.classList.replace("active", "inactive");
+buttonMyDashboard.addEventListener("click", e => {
+    buttonHome.classList.replace("active", "inactive");
+    buttonMyDashboard.classList.replace("inactive", "active");
+    buttonPeople.classList.replace("active", "inactive");
+    buttonPerformance.classList.replace("active", "inactive");
 })
 
-people.addEventListener("click", e => {
-    home.classList.replace("active", "inactive");
-    myDashboard.classList.replace("active", "inactive");
-    people.classList.replace("inactive", "active");
-    performance.classList.replace("active", "inactive");
+buttonPeople.addEventListener("click", e => {
+    buttonHome.classList.replace("active", "inactive");
+    buttonMyDashboard.classList.replace("active", "inactive");
+    buttonPeople.classList.replace("inactive", "active");
+    buttonPerformance.classList.replace("active", "inactive");
 })
 
-performance.addEventListener("click", e => {
-    home.classList.replace("active", "inactive");
-    myDashboard.classList.replace("active", "inactive");
-    people.classList.replace("active", "inactive");
-    performance.classList.replace("inactive", "active");
+buttonPerformance.addEventListener("click", e => {
+    buttonHome.classList.replace("active", "inactive");
+    buttonMyDashboard.classList.replace("active", "inactive");
+    buttonPeople.classList.replace("active", "inactive");
+    buttonPerformance.classList.replace("inactive", "active");
+})
+
+// Showpage
+
+const home = document.querySelector("#home")
+const myDashboard = document.querySelector("#myDashboard")
+const people = document.querySelector("#people")
+const performance = document.querySelector("#performance")
+
+
+buttonHome.addEventListener("click", e => {
+    home.classList.replace("inactiveShowPlace", "activeShowPlace");
+    myDashboard.classList.replace("activeShowPlace", "inactiveShowPlace");
+    people.classList.replace("activeShowPlace", "inactiveShowPlace");
+    performance.classList.replace("activeShowPlace", "inactiveShowPlace");
+})
+
+buttonMyDashboard.addEventListener("click", e => {
+    home.classList.replace("activeShowPlace", "inactiveShowPlace");
+    myDashboard.classList.replace("inactiveShowPlace", "activeShowPlace");
+    people.classList.replace("activeShowPlace", "inactiveShowPlace");
+    performance.classList.replace("activeShowPlace", "inactiveShowPlace");
+})
+
+buttonPeople.addEventListener("click", e => {
+    home.classList.replace("activeShowPlace", "inactiveShowPlace");
+    myDashboard.classList.replace("activeShowPlace", "inactiveShowPlace");
+    people.classList.replace("inactiveShowPlace", "activeShowPlace");
+    performance.classList.replace("activeShowPlace", "inactiveShowPlace");
+})
+
+buttonPerformance.addEventListener("click", e => {
+    home.classList.replace("activeShowPlace", "inactiveShowPlace");
+    myDashboard.classList.replace("activeShowPlace", "inactiveShowPlace");
+    people.classList.replace("activeShowPlace", "inactiveShowPlace");
+    performance.classList.replace("inactiveShowPlace", "activeShowPlace");
 })
 
